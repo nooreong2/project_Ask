8766ce7021d1862fdaca51f458af517ce5879f99
nooreong2 <97031069+nooreong2@users.noreply.github.com>
2022-02-07 17:11:50 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {SiNaver} from "react-icons/si";
+import {RiChat1Line} from "react-icons/ri";
+import {AiFillGithub} from "react-icons/ai";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div className="App">
+            <div className="container">
+                <div className="logo"></div>
+                <div className="total_login">통합 로그인</div>
+                <div className="input-container">
+                    <div className="input-wrap">
+                        <input type="text" className="input-id" placeholder="아이디 입력"/>
+                    </div>
+                    <div className="input-wrap">
+                        <input type="text" className="input-password" placeholder="비밀번호 8자리 입력"/>
+                    </div>
+                </div>
+                <div className="social_login">
+                    <button>
+                        <div className="icon"><SiNaver/></div>
+                        <span className="button_name">네이버 아이디로 로그인</span>
+                    </button>
+                    <button>
+                        <div className="icon"><RiChat1Line/></div>
+                        <span>카카오 계정으로 로그인</span>
+                    </button>
+                    <button>
+                        <div className="icon"><AiFillGithub/></div>
+                        <span>Github으로 로그인</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
